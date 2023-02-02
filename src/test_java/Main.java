f747ddddf2182aa80a8c75d8d80477a3c5293649
dashdev0129 <dashdev129129@gmail.com>
2023-02-02 13:34:05 -0500

    first added.

---
+++ src/test_java/Main.java
@@ -0,0 +1,10 @@
+package test_java;
+
+import java.time.LocalDateTime;
+
+public class Main {
+	public static void main(String[] args) {
+		int rand = (int)(Math.random()*100);
+		System.out.println(rand);
+	}
+}
